2b5dc0bf7c7fd9f30e2756c4ef44186ca7204901
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-28 17:51:23 -0700

    CS V11

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
                 num = float(input(f'Enter number {i+1}'))
                 numbers.append(num)
                 break
+            except ValueError:
+                print('Invalid Input, Try Again')
+                
                 
 
 
